009283290f8c69f0c2a9c02b0ef4a299f281c037
sunguolin <guolingege@qq.com>
2017-04-26 17:18:30 +0800

    Initial Commit

---
+++ DelayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelayDemo
+//
+//  Created by 孙国林 on 2017/4/26.
+//  Copyright © 2017年 孙国林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelayDemo
+//
+//  Created by 孙国林 on 2017/4/26.
+//  Copyright © 2017年 孙国林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
